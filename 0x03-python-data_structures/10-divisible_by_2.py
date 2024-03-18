550c8da0ba955b3d384d47c6fc0a32cb82515c7f
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2024-03-19 01:56:23 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return [True if x % 2 == 0 else False for x in my_list]
+    return [num % 2 == 0 for num in my_list]
 
 if __name__ == "__main__":
     my_list = [0, 1, 2, 3, 4, 5, 6]
