6f5d094f0380e933500f032898fcf3853b0b2e9a
Gee2424 <88376078+Gee2424@users.noreply.github.com>
2024-03-19 01:27:29 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+
+    Args:
+        my_list (list): The list from which to retrieve the element.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        The element at the specified index, or None if the index is out of range.
+    """
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
